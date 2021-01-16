e3a9929086ec9ef5957dcc8fbb0059b977f969fe
Ewan Brain <ewan.brain@gmail.com>
2021-01-16 10:56:36 +0000

    initial commit, api data loading

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <p> APP.vue test </p>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  data (){
+    return {
+      beers: [],
+
+    }
+  },
+  mounted(){
+    fetch('https://ghibliapi.herokuapp.com/films')
+    .then(res => res.json())
+    .then(beers => this.beers = beers);
+  },
+  components: {
+    
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
